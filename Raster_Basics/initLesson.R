ccf05018b6ee772849a39944cb11dd3f80a27b58
Johannes Heisig <johannes.heisig@gmail.com>
2018-12-07 13:55:09 +0100

    first lesson of 'Spatial Data in R'

---
+++ Raster_Basics/initLesson.R
@@ -0,0 +1,21 @@
+# Code placed in this file fill be executed every time the
+      # lesson is started. Any variables created here will show up in
+      # the user's working directory and thus be accessible to them
+      # throughout the lesson.
+
+list.of.packages <- c("raster")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+new.packages <- append(new.packages, list.of.packages[(list.of.packages %in% old.packages()[,"Package"])])
+if(length(new.packages)) install.packages(new.packages)
+
+.get_course_path <- function(){
+  tryCatch(swirl:::swirl_courses_dir(),
+           error = function(c) {file.path(find.package("swirl"),"Courses")}
+  )
+}
+
+
+library(raster)
+
+mt_rainier <- raster(file.path(.get_course_path(), 'The_SWIRL_demo_course',
+                               'My_first_demo_lesson','mt_rainier.tif'))
