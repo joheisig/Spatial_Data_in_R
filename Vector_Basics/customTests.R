da77bd9f0275d153f8b04f65c84f4480034a0ef8
Johannes Heisig <johannes.heisig@gmail.com>
2019-01-14 18:24:44 +0100

    added a few more tests

--- Vector_Basics/customTests.R
+++ Vector_Basics/customTests.R
@@ -20,4 +20,33 @@
            ok <- all(t1, t2)
          }, silent = TRUE)
          exists('ok') && isTRUE(ok)
+         options(warn=-1)
+         rm(ok, t1, t2, zf)
+         options(warn=0)
        }
+       
+       test_graphic_par <- function() {
+         try({
+           p <- par()
+           t1 <- identical(as.numeric(p$mfrow), c(1,1))
+           ok <- all(t1)
+         }, silent = TRUE)
+         exists('ok') && isTRUE(ok)
+         options(warn=-1)
+         rm(ok, t1)
+         options(warn=0)
+       }
+       
+       test_subset2 <- function() {
+         try({
+           zh <- get('z.hospitals', globalenv())
+           t1 <- identical(as.numeric(nrow(zh)), 18)
+           t2 <- identical(as.character(unique(zh$type)), 'hospital')
+           ok <- all(t1, t2)
+         }, silent = TRUE)
+         exists('ok') && isTRUE(ok)
+         options(warn=-1)
+         rm(ok, t1, t2, zh)
+         options(warn=0)
+       }
+       
