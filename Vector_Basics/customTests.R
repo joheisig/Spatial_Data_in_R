c042d86d2cd9b7d715f1cbbb2aaf495ea552dd35
Johannes Heisig <johannes.heisig@gmail.com>
2019-01-15 18:16:47 +0100

    updated customTests for vector basics

--- Vector_Basics/customTests.R
+++ Vector_Basics/customTests.R
@@ -1,52 +1,168 @@
 # Put custom tests in this file.
-      
+
       # Uncommenting the following line of code will disable
       # auto-detection of new variables and thus prevent swirl from
       # executing every command twice, which can slow things down.
-      
+
        AUTO_DETECT_NEWVAR <- FALSE
-      
+
       # However, this means that you should detect user-created
       # variables when appropriate. The answer test, creates_new_var()
       # can be used for for the purpose, but it also re-evaluates the
       # expression which the user entered, so care must be taken.
-       
-       
-       test_subset <- function() {
-         try({
-           zf <- get('z.forests', globalenv())
-           t1 <- identical(as.numeric(nrow(zf)), 1641)
-           t2 <- identical(as.character(unique(zf$type)), 'forest')
-           ok <- all(t1, t2)
-         }, silent = TRUE)
-         exists('ok') && isTRUE(ok)
-         options(warn=-1)
-         rm(ok, t1, t2, zf)
-         options(warn=0)
-       }
-       
-       test_graphic_par <- function() {
-         try({
-           p <- par()
-           t1 <- identical(as.numeric(p$mfrow), c(1,1))
-           ok <- all(t1)
-         }, silent = TRUE)
-         exists('ok') && isTRUE(ok)
-         options(warn=-1)
-         rm(ok, t1)
-         options(warn=0)
-       }
-       
-       test_subset2 <- function() {
-         try({
-           zh <- get('z.hospitals', globalenv())
-           t1 <- identical(as.numeric(nrow(zh)), 18)
-           t2 <- identical(as.character(unique(zh$type)), 'hospital')
-           ok <- all(t1, t2)
-         }, silent = TRUE)
-         exists('ok') && isTRUE(ok)
-         options(warn=-1)
-         rm(ok, t1, t2, zh)
-         options(warn=0)
-       }
-       
+
+getState <- function(){
+environment(sys.function(1))$e
+}
+
+getVal <- function(){
+getState()$val
+}
+
+getExpr <- function(){
+getState()$expr
+}
+
+one_is_true <- function(x){
+    ok <- TRUE %in% x
+    exists('ok') && isTRUE(ok)
+}
+
+test_subset <- function() {
+ try({
+   zf <- get('z.forests', globalenv())
+   t1 <- identical(as.numeric(nrow(zf)), 1641)
+   t2 <- identical(as.character(unique(zf$type)), 'forest')
+   ok <- all(t1, t2)
+ }, silent = TRUE)
+ exists('ok') && isTRUE(ok)
+ options(warn=-1)
+ rm(ok, t1, t2, zf)
+ options(warn=0)
+}
+
+test_graphic_par <- function() {
+ try({
+   p <- par()
+   t1 <- identical(as.numeric(p$mfrow), c(1,1))
+   ok <- all(t1)
+ }, silent = TRUE)
+ exists('ok') && isTRUE(ok)
+ options(warn=-1)
+ rm(ok, t1)
+ options(warn=0)
+}
+
+test_plot_forest <- function() {
+try({
+   ex0 <- as.character(getExpr())
+   ex1 <- sub('"', "", sub('"', "", ex0))
+   ex2 <- sub("'", "", sub("'", "", ex1))
+
+   t1 <- grepl('plot', ex2)
+   t2 <- grepl('z.forests', ex2)
+   t3 <- grepl('col', ex2)
+   t4 <- grepl('lightgreen', ex2)
+
+   ok <- all(t1, t2, t3, t4)
+ }, silent = TRUE)
+ exists('ok') && isTRUE(ok)
+ options(warn=-1)
+ rm(ok, t1, t2, t3, t4, ex1, ex2, ex0)
+ options(warn=0)
+}
+
+test_area_forest <- function(){
+    try({
+       ex0 <<- as.numeric(getVal())
+
+       t1 <- ex0 > 540
+       t2 <- ex0 < 550
+
+       ok <- all(t1, t2)
+     }, silent = TRUE)
+     exists('ok') && isTRUE(ok)
+     options(warn=-1)
+     rm(ok, t1, t2)
+     options(warn=0)
+}
+
+test_plot_lakes <- function() {
+try({
+   ex0 <- as.character(getExpr())
+   ex1 <- sub('"', "", sub('"', "", ex0))
+   ex2 <- sub("'", "", sub("'", "", ex1))
+
+   t1 <- grepl('plot', ex2)
+   t2 <- grepl('z.lakes', ex2)
+   t3 <- grepl('add', ex2)
+   t4 <- grepl('lightblue', ex2)
+
+   ok <- all(t1, t2, t3, t4)
+ }, silent = TRUE)
+ exists('ok') && isTRUE(ok)
+ options(warn=-1)
+ rm(ok, t1, t2, t3, t4, ex1, ex2, ex0)
+ options(warn=0)
+}
+
+test_lines_func <- function() {
+try({
+   ex0 <- as.character(getExpr())
+   ex1 <- sub('"', "", sub('"', "", ex0))
+   ex2 <- sub("'", "", sub("'", "", ex1))
+
+   t1 <- grepl('lines', ex2)
+
+   tt2 <- c(grepl('z.streams', ex2), grepl('z.roads', ex2), grepl('z', ex2))
+   t2 <- one_is_true(tt2)
+
+   tt3 <- c(grepl('royalblue', ex2), grepl('orange', ex2), grepl('red', ex2))
+   t3 <- one_is_true(tt3)
+
+   t4 <- grepl('lwd', ex2)
+
+   ok <- all(t1, t2, t3, t4)
+ }, silent = TRUE)
+ exists('ok') && isTRUE(ok)
+ options(warn=-1)
+ rm(ok, t1, t2, t3, t4, tt2, tt3,  ex1, ex2, ex0)
+ options(warn=0)
+}
+
+test_points_func <- function() {
+try({
+   ex0 <- as.character(getExpr())
+   ex1 <- sub('"', "", sub('"', "", ex0))
+   ex2 <- sub("'", "", sub("'", "", ex1))
+
+   t1 <- grepl('points', ex2)
+   t2 <- grepl('pch', ex2)
+   t3 <- grepl('15', ex2)
+   t4 <- grepl('z.hospitals', ex2)
+
+   ok <- all(t1, t2, t3, t4)
+ }, silent = TRUE)
+ exists('ok') && isTRUE(ok)
+ options(warn=-1)
+ rm(ok, t1, t2, t3, t4, ex1, ex2, ex0)
+ options(warn=0)
+}
+
+test_subset2 <- function() {
+ try({
+   zh <- get('z.hospitals', globalenv())
+   t1 <- identical(as.numeric(nrow(zh)), 18)
+   t2 <- identical(as.character(unique(zh$type)), 'hospital')
+   ok <- all(t1, t2)
+ }, silent = TRUE)
+ exists('ok') && isTRUE(ok)
+ options(warn=-1)
+ rm(ok, t1, t2, zh)
+ options(warn=0)
+}
+
+
+
+
+
